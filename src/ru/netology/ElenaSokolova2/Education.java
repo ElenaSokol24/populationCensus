a972ca02247da87431b8843350897c08b86f89e0
ElenaSokol24 <k24.elena@rambler.ru>
2022-02-28 17:04:18 +0300

    Initial commit

---
+++ src/ru/netology/ElenaSokolova2/Education.java
@@ -0,0 +1,9 @@
+package ru.netology.ElenaSokolova2;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
+
---
+++ src/ru/netology/ElenaSokolova2/Sex.java
@@ -0,0 +1,8 @@
+package ru.netology.ElenaSokolova2;
+
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
+
